f000956e55a2f28760acc8bfe34592e4c1970465
uribo <ur1b0.dev@gmail.com>
2020-07-31 10:32:55 +0900

    LEDとスイッチの定義と初期化を追加

--- usb_co2.ino
+++ usb_co2.ino
@@ -1,4 +1,18 @@
+#define PIN_LED_RED 0
+#define PIN_LED_GREEN 1
+#define LED_ON LOW
+#define LED_OFF HIGH
+#define PIN_SW1 3
+#define PIN_SW2 2
+
 void setup() {
+  digitalWrite(PIN_LED_RED, LED_OFF);
+  digitalWrite(PIN_LED_GREEN, LED_OFF);
+  pinMode(PIN_LED_RED, OUTPUT);
+  pinMode(PIN_LED_GREEN, OUTPUT);
+  pinMode(PIN_SW1, INPUT);
+  pinMode(PIN_SW2, INPUT);
+
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect
@@ -14,5 +28,5 @@ void loop() {
   Serial.print("co2=");
   Serial.println(value, DEC);
 
-  delay(900);
+  delay(1000);
 }
