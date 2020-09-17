67493704bbaa7be3b1456e8268047d36436af092
uribo <ur1b0.dev@gmail.com>
2020-09-17 18:38:04 +0900

    外付けLEDを無くしてボード上のLEDを使用するようにした

--- usb_co2.ino
+++ usb_co2.ino
@@ -1,27 +1,23 @@
-#define PIN_LED_RED 0
-#define PIN_LED_GREEN 1
 #define LED_ON LOW
 #define LED_OFF HIGH
 #define PIN_SW1 3
 #define PIN_SW2 2
 
 void setup() {
-  digitalWrite(PIN_LED_RED, LED_OFF);
-  digitalWrite(PIN_LED_GREEN, LED_OFF);
-  pinMode(PIN_LED_RED, OUTPUT);
-  pinMode(PIN_LED_GREEN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LED_OFF);
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(PIN_SW1, INPUT);
   pinMode(PIN_SW2, INPUT);
 
   usb_mhz14a_init();
 /*
   if( digitalRead( PIN_SW1 ) ){
-    digitalWrite(PIN_LED_RED, LED_ON);
+    digitalWrite(LED_BUILTIN, LED_ON);
     usb_mhz14a_zero_calibration();
-    digitalWrite(PIN_LED_RED, LED_OFF);
+    digitalWrite(LED_BUILTIN, LED_OFF);
   }
 */
-  digitalWrite(PIN_LED_GREEN, LED_ON);
+  digitalWrite(LED_BUILTIN, LED_ON);
 
   Serial.begin(9600);
   while (!Serial) {
